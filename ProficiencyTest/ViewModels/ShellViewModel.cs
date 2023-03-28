494069c7d51115d1d1f487360c760cf74c84a35a
Tan Jui Cheng <jctan815@gmail.com>
2023-03-28 20:56:29 +0800

    Implemented functionality to cancel button as closing of this application.

--- ProficiencyTest/ViewModels/ShellViewModel.cs
+++ ProficiencyTest/ViewModels/ShellViewModel.cs
@@ -4,10 +4,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace ProficiencyTest.ViewModels
 {
     public class ShellViewModel : Screen
     {
+        public void Cancel()
+        {            
+            TryCloseAsync();
+        }
     }
 }
