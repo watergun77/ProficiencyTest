47d919f5e165534d5d4c6c95eb8a01da94e53954
Tan Jui Cheng <jctan815@gmail.com>
2023-03-28 18:26:29 +0800

    Added Caliburn.Micro framework to support MVVM.

---
+++ ProficiencyTest/Bootstrapper.cs
@@ -0,0 +1,18 @@
+﻿using Caliburn.Micro;
+using ProficiencyTest.ViewModels;
+using System.Windows;
+
+namespace ProficiencyTest
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewForAsync<ShellViewModel>();
+        }
+    }
+}
